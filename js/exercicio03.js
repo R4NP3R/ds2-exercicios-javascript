76df480a294e0f345e2151eb3be9587d1e8fea29
R4NP3R <ssrafa.2014@gmail.com>
2026-08-25 18:39:49 -0300

    feat: coleta e apresenta dados do aluno

---
+++ js/exercicio03.js
@@ -0,0 +1,12 @@
+function exercicio3() {
+  let nome = prompt("Insira o seu nome: ");
+  let idade = prompt("Insira sua idade: ");
+  let curso = prompt("Insira o seu curso: ");
+  let cidade = prompt("Insira a sua cidade: ");
+
+
+  console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e estou cursando ${curso}`);
+
+  console.log("Olá, meu nome é " + nome + ", tenho " + idade + " anos, moro em "+ cidade + " e estou cursando " + curso);
+
+}
